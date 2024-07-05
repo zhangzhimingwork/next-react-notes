89a8e1ea6901c3149a568ceff724ac73d5009613
Mac <mac@mac.local>
2024-07-05 23:26:50 +0800

    项目初始化

---
+++ app/components/Siderbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+import { getAllNotes } from '@/lib/redis.js'
+import SidebarNoteList from './SidebarNoteList'
+
+export default async function Sidebar() {
+  const notes = await getAllNotes()
+  return (
+    <>
+      <section className='col sidebar'>
+        <Link href={'/'} className='link--unstyled'>
+          <section className='sidebar-header'>
+            <img
+              className='logo'
+              src='/logo.svg'
+              width='22px'
+              height='20px'
+              alt=''
+              role='presentation'
+            />
+            <strong>React Notes</strong>
+          </section>
+        </Link>
+        <section className='sidebar-menu' role='menubar'>
+          {/* SideSearchField */}
+        </section>
+        <nav><SidebarNoteList notes={notes} /></nav>
+      </section>
+    </>
+  )
+}
